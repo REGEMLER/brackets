2a6bacdd737504ba71a2173902a1d94f967decf6
Yar Night <yaroslav239@gmail.com>
2023-08-13 20:10:29 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const r = /\(\)|\[\]|\{\}|\|\||12|34|56|77|88/gm;
+  let num1 = "";
+  let num2 = str; 
+  while (num2 !== num1) {
+    num1 = num2,
+    num2 = num2.replace(r, '');
+  }
+
+  return num1 == '';
 }
